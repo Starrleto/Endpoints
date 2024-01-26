d927e0cb15815d2d37f915028095dcd5a0c74084
Starrleto <ellierasuli@gmail.com>
2024-01-26 10:22:34 -0800

    ok

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 using RasuliEEndpoints.Services;
 using RasuliEEndpoints.Services.Questions;
+using RasuliEEndpoints.Services.SayHello;
 using RasuliEEndpoints.Services.Sums;
 using RasuliEEndpoints.Services.Sums.Results;
+using RasuliEEndpointsD2.Controllers;
+using RasuliEEndpointsD2.Services;
+using RasuliEEndpointsD2.Services.Madlib;
+using RasuliEEndpointsD2.Services.OddOrEven;
+using RasuliEEndpointsD2.Services.ReverseNum;
+using RasuliEEndpointsD2.Services.ReverseString;
+using RasuliEEndpointsD3.Services.MagicBall;
+using RasuliEEndpointsD3.Services.Restauraunt;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +23,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IQuestionsService, QuestionService>();
 builder.Services.AddScoped<IResultsService, ResultsService>();
 builder.Services.AddScoped<ISumService, SumsService>();
+builder.Services.AddScoped<IMadlibService, MadlibService>();
+builder.Services.AddScoped<IOddEvenService, OddEvenService>();
+builder.Services.AddScoped<iSayHelloService, SayHelloService>();
+builder.Services.AddScoped<IReverseNumService, ReverseNumService>();
+builder.Services.AddScoped<IReverseStringService, ReverseStringService>();
+builder.Services.AddScoped<IMagicBallService, MagicBallService>();
+builder.Services.AddScoped<IRestaurauntPicker, RestaurauntPicker>();
 
 var app = builder.Build();
 
